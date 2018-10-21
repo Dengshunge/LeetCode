541d68df6333114eca14ac9aa93c0f09b4d8616a
Deng shunge <dengshunge2013@163.com>
2018-10-21 16:45:47 +0800

    Create x 的平方根.cpp

---
+++ x 的平方根.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <utility>
+#include <unordered_map>
+#include <functional>
+using namespace std;
+
+/*
+两种方法
+第一种，二分法，将mid的平方与x比较，然后修改low和high的值，逐步逼近；
+第二种，牛顿法，f(x)=i^2-x=0，求f(x)=0的根，所以x_k+1 = x_k - f(x_k)/f`(x_k)
+*/
+
+class Solution {
+public:
+	int mySqrt(int x) 
+	{
+		int low = 0, high = x;
+		while (low <= high)
+		{
+			long mid = (low + high) >> 1;
+			long temp = mid*mid;
+			if (temp == x)
+				return mid;
+			else if (temp < x)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return high;
+	}
+
+	int mySqrt2(int x)
+	{
+		double i_last = 0;
+		double i_new = 2;
+		while (abs(i_new - i_last) >0.1)
+		{
+			i_last = i_new;
+			i_new = (i_new + x / i_new) / 2;
+		}
+		return int(i_new);
+	}
+};
+
+int main() 
+{
+	Solution a;
+	cout << a.mySqrt2(INT_MAX) << endl;
+
+
+
+	system("pause");
+	return 0;
+}
+
+
+
