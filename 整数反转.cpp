0a80cf38fde769de2f62ee6cab640c8f8ba582d7
Deng shunge <dengshunge2013@163.com>
2019-02-28 21:02:03 +0800

    Create 整数反转.cpp

---
+++ 整数反转.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <utility>
+#include <queue>
+#include <stack>
+using namespace std;
+
+
+class Solution {
+public:
+	int reverse(int x) {
+		bool flag = (x >= 0) ? true : false;
+		int res = 0;
+		while (x != 0)
+		{
+			int tmp = x % 10;
+			if ((flag && (res>(INT_MAX - tmp) / 10)) || (!flag && (res<(INT_MIN - tmp) / 10)))
+				return 0;
+			res = res * 10 + tmp;
+			x /= 10;
+		}
+		return res;
+	}
+};
+
+int main() 
+{
+	Solution a;
+	cout << a.reverse(INT_MAX) << endl;
+
+
+
+	
+
+
+	system("pause");
+	return 0;
+}
