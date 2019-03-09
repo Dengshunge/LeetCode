3ad3bad2c0495099f4c22b6b153e7fc135e44fa6
Deng shunge <dengshunge2013@163.com>
2019-03-09 12:00:33 +0800

    Create 三数之和.cpp

---
+++ 三数之和.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <utility>
+#include <queue>
+#include <stack>
+using namespace std;
+
+
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		set<vector<int>> res;
+		if (nums.size() < 3)
+			//返回空的vector
+			return vector<vector<int>>(res.begin(), res.end());
+		sort(nums.begin(), nums.end());
+		//这里num从[0,nums.size()-2)，因为[nums.size()-2到nums.size()-1只有2个数，不符合3-sum
+		for (auto num = nums.begin(); num < nums.end() - 2; ++num)
+		{
+			if (*num > 0)
+				break;
+			//跳过重复的数字
+			if (num != nums.begin() && *num == *(num - 1))
+				continue;
+			auto left = num + 1, right = nums.end() - 1;
+			while (left<right)
+			{
+				int tmp = *num + *left + *right;
+				if (tmp == 0)
+				{
+					res.insert({ *num,*left,*right });
+					//跳过重复的数字
+					//这里不能直接break，因为可能还有其他组合加起来等于0
+					while (left < right&&*left == *(left + 1))
+						++left;
+					++left;
+					while (left < right&&*right == *(right - 1))
+						--right;
+					--right;
+				}
+				else if (tmp > 0)
+					--right;
+				else
+					++left;
+			}
+		}
+		return vector<vector<int>>(res.begin(), res.end());
+	}
+};
+
+int main() 
+{
+	vector<int> nums{1,-1};
+	Solution a;
+	auto b = a.threeSum(nums);
+	for (auto m : b)
+	{
+		for (auto n : m)
+			cout << n << " ";
+		cout << endl;
+	}
+
+
+	
+
+
+	system("pause");
+	return 0;
+}
