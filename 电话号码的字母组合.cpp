db98f941ee91f87a6551eee36ea36d75409a2191
Deng shunge <dengshunge2013@163.com>
2019-03-18 16:13:52 +0800

    Update 电话号码的字母组合.cpp

--- 电话号码的字母组合.cpp
+++ 电话号码的字母组合.cpp
@@ -36,6 +36,70 @@ private:
 	}
 };
 
+//
+class Solution2 {
+public:
+	vector<string> letterCombinations(string digits) 
+	{
+		vector<string> res;
+		if (digits.empty())
+			return res;
+		string path;
+		letterCombinations(res, path, digits);
+		return res;
+	}
+
+	vector<string> letterCombinations_network(string digits)
+	{
+		vector<string>numToString{ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
+		if (digits.empty())
+			return {};
+		queue<string> re;
+		re.push("");
+		for (int i = 0; i < digits.size(); ++i)
+		{
+			int length = re.size();
+			for (int j = 0; j < length; ++j)
+			{
+				string tmp = re.front();
+				re.pop();
+				for (auto c : numToString[digits[i] - '0'])
+				{
+					re.push(tmp + c);
+				}
+			}
+		}
+		vector<string> res;
+		while (!re.empty())
+		{
+			res.push_back(re.front());
+			re.pop();
+		}
+		return res;
+	}
+
+private:
+	vector<string>numToString{ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
+	void letterCombinations(vector<string> &res, string path, string digits)
+	{
+		if (digits.empty())
+		{
+			res.push_back(path);
+			return;
+		}
+		if (digits[0] != '1')
+		{
+			string tmpString = numToString[digits[0] - '0'];
+			for (int i = 0; i < tmpString.size(); ++i)
+			{
+				letterCombinations(res, path + tmpString[i], digits.substr(1));
+			}
+		}
+		else
+			letterCombinations(res, path, digits.substr(1));
+	}
+};
+
 int main()
 {
 	Solution a;
