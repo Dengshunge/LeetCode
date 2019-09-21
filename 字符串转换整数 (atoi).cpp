f6f1d10fd0da5b27f29ce707f0f13dd81f1c5f8d
Deng shunge <dengshunge2013@163.com>
2019-09-21 11:09:00 +0800

    Update 字符串转换整数 (atoi).cpp

--- 字符串转换整数 (atoi).cpp
+++ 字符串转换整数 (atoi).cpp
@@ -43,6 +43,39 @@ public:
 		}
 		return res;
 	}
+	
+	//
+	int myAtoi2(string str)
+	{
+		int positive = 1;
+		int i = 0;
+		int res = 0;
+		//去除空格
+		while (str[i] == ' ')
+		{
+			++i;
+		}
+		//判断第一个字符
+		if (str[i] == '-'|| str[i] == '+')
+		{
+			if (str[i] == '-')
+				positive = -1;
+			++i;
+		}
+		//判断数字
+		for (; i < str.size(); ++i)
+		{
+			int num = str[i] - '0';
+			if (num > 9 || num < 0)
+				return res;
+			if (positive == 1 && res > (INT_MAX - positive * num) / 10)
+				return INT_MAX;
+			if (positive == -1 && res < (INT_MIN - positive * num) / 10)
+				return INT_MIN;
+			res = res * 10 + positive * num;
+		}
+		return res;
+	}
 };
 
 int main() 
