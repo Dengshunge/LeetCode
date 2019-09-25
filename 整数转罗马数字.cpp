f5c58b5ef6b409aeca8f48daf225bbeccfa9eb09
Deng shunge <dengshunge2013@163.com>
2019-09-25 19:27:56 +0800

    Update 整数转罗马数字.cpp

--- 整数转罗马数字.cpp
+++ 整数转罗马数字.cpp
@@ -90,6 +90,27 @@ public:
 		return roman;
 	}
 	
+	//update 2019.9.25
+	string intToRoman(int num) {
+		map<int, string> mapRom = { { 1,"I" },{ 4,"IV" },{ 5,"V" },{ 9,"IX" },{ 10,"X" },{ 40,"XL" },{ 50,"L" },{ 90,"XC" },{ 100,"C" },{ 400,"CD" },{ 500,"D" },{ 900,"CM" },{ 1000,"M" } };
+		map<int, string>::reverse_iterator  r_iter;
+
+		string ret;
+
+		r_iter = mapRom.rbegin();
+		while (r_iter != mapRom.rend())
+		{
+			if (num >= r_iter->first)
+			{
+				ret += r_iter->second;
+				num -= r_iter->first;
+			}
+			else
+				r_iter++;
+		}
+		return ret;
+	}
+	
 private:
 	//4是说明有4个，第一个10说明每个里面有10个字符串，第二个10说明每个字符串有10个字符
 	const char Roman[4][10][10]={
