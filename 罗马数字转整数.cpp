6b6e1cd5fe6cec8206f9b001f64d92e459426f42
Deng shunge <dengshunge2013@163.com>
2019-09-26 20:45:19 +0800

    Update 罗马数字转整数.cpp

--- 罗马数字转整数.cpp
+++ 罗马数字转整数.cpp
@@ -187,6 +187,28 @@ public:
 	}
 };
 
+//update 2019.9.26
+class Solution3 {
+public:
+	int romanToInt(string s) 
+	{
+		map<char, int> RomanMap = { { 'I',1 },{ 'V',5 },{ 'X',10 },{ 'L',50 },{ 'C',100 },{ 'D',500 },{ 'M',1000 } };
+		int res = RomanMap[s[s.size() - 1]];
+		for (int i = s.size()-2; i >=0; --i)
+		{
+			if (RomanMap[s[i]] < RomanMap[s[i + 1]])
+			{
+				res -= RomanMap[s[i]];
+			}
+			else
+			{
+				res += RomanMap[s[i]];
+			}
+		}
+		return res;
+	}
+};
+
 
 int main()
 {
