15a57c7cbcc5fbfcf11e0bbb4fc17125d726a2ad
Deng shunge <dengshunge2013@163.com>
2019-10-16 19:28:51 +0800

    Update 搜索旋转排序数组.cpp

--- 搜索旋转排序数组.cpp
+++ 搜索旋转排序数组.cpp
@@ -53,6 +53,48 @@ public:
 	}
 };
 
+//
+class Solution2 {
+public:
+	int search(vector<int>& nums, int target) 
+	{
+		if (nums.empty())
+			return -1;
+		int left = 0, right = nums.size() - 1;
+		while (left<right)
+		{
+			int mid = (left + right) >> 1;
+			if (nums[mid] > nums[right])
+				left = mid + 1;
+			else
+				right = mid;
+		}
+		if (left == 0)
+		{
+			left = 0;
+			right = nums.size() - 1;
+		}
+		else if (target >= nums[0])
+		{
+			left = 0;
+			right = right - 1;
+		}
+		else
+			right = nums.size() - 1;
+		while (left<=right)
+		{
+			int mid = (left + right) >> 1;
+			if (nums[mid] == target)
+				return mid;
+			if (target < nums[mid])
+				right = mid - 1;
+			else
+				left = mid + 1;
+		}
+		return -1;
+	}
+};
+
 int main()
 {
 	vector<int> nums{ 3,1 };
