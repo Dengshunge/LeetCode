2235c98e49a14896b2ffd9d8135ff7b8039470fb
Deng shunge <dengshunge2013@163.com>
2019-10-20 10:14:02 +0800

    Update 搜索插入位置.cpp

--- 搜索插入位置.cpp
+++ 搜索插入位置.cpp
@@ -35,6 +35,21 @@ public:
 			return mid + 1;
 		}
 	}
+	
+	//update 2019.10.20
+	int searchInsert(vector<int>& nums, int target) 
+	{
+		int left = 0, right = nums.size();
+		while (left < right)
+		{
+			unsigned int mid = (left + right) >> 1;
+			if (nums[mid] < target)
+				left = mid + 1;
+			else
+				right = mid;
+		}
+		return left;
+	}
 };
 
 
