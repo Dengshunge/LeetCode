f21316905e5a516fdb23ea4200087d8bc0048a9a
Deng shunge <dengshunge2013@163.com>
2019-10-26 20:34:53 +0800

    Update 最小路径和.cpp

--- 最小路径和.cpp
+++ 最小路径和.cpp
@@ -38,6 +38,20 @@ public:
 		}
 		return res[m - 1][n - 1];
 	}
+	
+	//update 2019.10.26
+	int minPathSum(vector<vector<int>>& grid) 
+	{
+		int m = grid.size(), n = grid[0].size();
+		for (int i = 1; i < n; ++i)
+			grid[0][i] += grid[0][i - 1];
+		for (int i = 1; i < m; ++i)
+			grid[i][0] += grid[i - 1][0];
+		for (int i = 1; i<m; ++i)
+			for (int j = 1; j < n; ++j)
+				grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+		return grid[m - 1][n - 1];
+	}
 };
 
 int main() 
