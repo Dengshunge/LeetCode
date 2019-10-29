eccfb692c6dc76ba00c6d0162d1820817c7737e3
Deng shunge <dengshunge2013@163.com>
2019-10-29 19:14:19 +0800

    Update 不同路径.cpp

--- 不同路径.cpp
+++ 不同路径.cpp
@@ -61,6 +61,16 @@ public:
 		}
 		return res[m - 1][n - 1];
 	}
+	
+	//update 2019.10.29
+	int uniquePaths(int m, int n)
+	{
+		vector<vector<int>> dp(m, vector<int>(n, 1));
+		for (int i = 1; i<m; i++)
+			for (int j = 1; j<n; ++j)
+				dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+		return dp[m - 1][n - 1];
+	}
 };
 
 int main() 
