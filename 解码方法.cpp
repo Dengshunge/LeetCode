bac8ad9a23fdb621842057fc29a8d7342bc649b8
Deng shunge <dengshunge2013@163.com>
2019-11-02 18:22:34 +0800

    Update 解码方法.cpp

--- 解码方法.cpp
+++ 解码方法.cpp
@@ -50,6 +50,59 @@ public:
 		}
 		return times.back();
 	}
+	
+	//update 2019.11.2
+	int numDecodings(string s) 
+	{
+		if (s[0] == '0')
+			return 0;
+		if (s.size() < 2)
+			return 1;
+		vector<int> dp(s.size(), 0);
+		dp[0] = 1;
+		if (s[1] == '0')
+		{
+			if (s[0] > '2')
+				return 0;
+			else
+				dp[1] = 1;
+		}
+		else
+		{
+			if (stoi(s.substr(0, 2)) <= 26)
+				dp[1] = 2;
+			else
+				dp[1] = 1;
+		}
+		for (int i = 2; i < s.size(); ++i)
+		{
+			if (s[i] == '0')
+			{
+				if (s[i - 1] == '0')
+					return 0;
+				else
+				{
+					if (stoi(s.substr(i - 1, 2)) <= 26)
+						dp[i] = dp[i - 2];
+					else
+						return 0;
+				}
+			}
+			else
+			{
+				if (s[i - 1] == '0')
+					dp[i] = dp[i - 1];
+				else
+				{
+					if (stoi(s.substr(i - 1, 2)) <= 26)
+						dp[i] = dp[i - 1] + dp[i - 2];
+					else
+						dp[i] = dp[i - 1];
+				}
+			}
+		}
+		return dp.back();
+	}
 };
 
 
