d43f3ad72917d498a6cb21a9d297b25b7e668a87
Deng shunge <dengshunge2013@163.com>
2019-11-09 21:32:39 +0800

    Update 旋转图像.cpp

--- 旋转图像.cpp
+++ 旋转图像.cpp
@@ -46,6 +46,26 @@ public:
 	}
 };
 
+//
+class Solution3 {
+public:
+	void rotate(vector<vector<int>>& matrix) 
+	{
+		int size = matrix.size();
+		for (int i = 0; i < size; ++i)
+		{
+			for (int j = i; j < size; ++j)
+			{
+				swap(matrix[i][j], matrix[j][i]);
+			}
+		}
+		for (int i = 0; i < size; ++i)
+		{
+			reverse(matrix[i].begin(), matrix[i].end());
+		}
+	}
+};
+
 int main()
 {
 	vector<vector<int>> matrix{ {1,2,3},{4,5,6},{7,8,9} };
